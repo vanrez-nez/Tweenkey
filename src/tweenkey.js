3557d1574f3aaa7f37d7875a763b34525f19e23e
Ivan Juarez <ivan@grupow.com>
2016-01-14 17:31:36 -0600

    Type accessories and small improvements.

--- src/tweenkey.js
+++ src/tweenkey.js
@@ -1,13 +1,23 @@
 
 var Tweenkey = Tweenkey || (function() {
 	"use strict";
-
+	
+	var rAF, cAF;
 	var tweens = [];
 	var m = Math;
 	var wnd = window;
+	var objectPrototypeStr = Object.prototype.toString;
 
 	function isFunction( object ) {
-		return Object.prototype.toString.call( object ) == '[object Array]';
+		return objectPrototypeStr.call( object ) == '[object Array]';
+	}
+
+	function isObject( object ) {
+		return objectPrototypeStr.call( object ) == '[object Object]';
+	}
+
+	function isNumber( object ) {
+		return objectPrototypeStr.call( object ) == '[object Number]';
 	}
 
 	function lerp( t, b, c, d ) {
@@ -41,7 +51,6 @@ var Tweenkey = Tweenkey || (function() {
 
 		// fire onUpdate notification
 		isFunction( tween.onUpdate ) && tween.onUpdate( tween )		
-	
 
 		if ( tween.elapsedTime >= tween.duration ) {
 			tween.running = false;
@@ -53,13 +62,14 @@ var Tweenkey = Tweenkey || (function() {
 	}
 
 	function addProps(tween, params, reverse) {
-		// extract remaining properties as values for tweening
 		for ( var p in params ) {
-			!tween[ p ] && tween.target[ p ] !== undefined && tween.props.push( {
-				name: p,
-				[ reverse ? 'to' : 'from' ]: tween.target[ p ],
-				[ reverse ? 'from' : 'to' ]: params[ p ]
-			} );
+			if ( !tween[ p ] && isNumber( tween.target[ p ] ) ) {
+				var prop = { name: p, 'f': tween.target[ p ], 't': params[ p ]};
+
+				// swap from and to values if reverse flag
+				reverse && ( prop.t = [ prop.f, prop.f = prop.t ][ 0 ] );
+				tween.props.push( prop );
+			}
 		}
 	}
 
@@ -69,13 +79,15 @@ var Tweenkey = Tweenkey || (function() {
 		tween.running = params.autoStart ? !!params.autoStart : true;
 		tween.ease = isFunction( params.ease ) ? params.ease : lerp;
 		tween.duration = m.max( 0, Number( duration ) || 0 );
+
+		// add remaining values inside params as properties
+		addProps( tween, params, tween.isTweenFrom );
 	}
 
 	Tween.prototype = {
 		set: function( target, duration, params ) {
-			if ( target ) {
+			if ( isObject( target ) ) {
 				initTween( this, target, duration, params );
-				addProps( this, params, this.isTweenFrom );
 				tweens.push( this );
 			} else {
 				console.warn( 'Invalid target:', target );
@@ -91,12 +103,12 @@ var Tweenkey = Tweenkey || (function() {
 				tweens.splice(idx, 1);
 			}
 		}
-		wnd.requestAnimationFrame( enterFrame );
+		rAF( enterFrame );
 	}
 
 	// taken from https://github.com/soulwire/sketch.js/blob/master/js/sketch.js
 	(function shimAnimationFrame() {
-		var vendors, a, b, c, rAF, cAF, i, now, dt, then;
+		var vendors, a, b, c, i, now, dt, then;
 
 		vendors = [ 'ms', 'moz', 'webkit', 'o' ];
 		a = 'AnimationFrame';
@@ -111,7 +123,7 @@ var Tweenkey = Tweenkey || (function() {
 			cAF = wnd[ vendors[ i ] + 'Cancel' + a ];
 		};
 
-		wnd[ b ] = rAF = rAF || function( callback ) {
+		rAF = rAF || function( callback ) {
 			now = +new Date();
 			dt = m.max( 0, 16 - ( now - then ) );
 			id = setTimeout( function() {
@@ -121,12 +133,12 @@ var Tweenkey = Tweenkey || (function() {
 			return id;
 		};
 
-		wnd[ c ] = cAF = cAF || function( id ) {
+		cAF = cAF || function( id ) {
 			clearTimeout( id );
 		};
 	})();
 
-	wnd.requestAnimationFrame( enterFrame );
+	rAF( enterFrame );
 
     return {
     	to: function() {
