3f4800b6f9c1367d491cc2cdcac9f4e759dec1e7
Iván Juárez Núñez <ivan@blindgazer.com>
2016-02-03 20:35:37 -0600

    (test): Adding tests for callbacks and linear interpolations for all tween objects.

--- tests/tweenkey.test.js
+++ tests/tweenkey.test.js
@@ -15,13 +15,6 @@ var tweenConstructors 	= [ 'set', 'from', 'to', 'fromTo' ];
 var tweenkeyMethods 	= [ 'autoUpdate', 'update', 'pauseAll', 'killAll', 'resumeAll', ];
 var tweenMethods 		= [ 'pause', 'resume', 'kill', 'delay' ];
 
-var basicParams = {
-	set: 	[ { x: 0 }, { x: 1 } ],
-	to: 	[ { x: 0 }, 1, { x: 1 } ],
-	from: 	[ { x: 1 }, 1, { x: 1} ],
-	fromTo: [ { x: 0 }, 1, { x: 1 }, { x: 2 } ]
-}
-
 Tweenkey.autoUpdate( false );
 
 describe( 'tweenkey', function() {
@@ -85,6 +78,14 @@ describe( 'tweenkey', function() {
 	} );
 
 	describe( 'Tween definitions', function() {
+
+		var basicParams = {
+			set: 	[ { x: 0 }, { x: 1 } ],
+			to: 	[ { x: 0 }, 1, { x: 1 } ],
+			from: 	[ { x: 1 }, 1, { x: 1} ],
+			fromTo: [ { x: 0 }, 1, { x: 1 }, { x: 2 } ]
+		}
+
 		it( 'Should respond to [pause, resume, kill, delay] methods', function() {
 			
 			tweenConstructors.forEach( function( cName ) {
@@ -164,6 +165,31 @@ describe( 'tweenkey', function() {
 			} );
 		} );
 
+		it( 'to: execute the [onComplete, onUpdate] callbacks', function( done ) {
+			var obj = { x: 0 };
+			Tweenkey.to( obj, 1, { x: 1, 
+				onUpdate: function( target ) {
+					expect( obj ).to.have.property( 'x' ).and.equal( 1 );
+				},
+				onComplete: function( target ) {
+					expect( obj ).to.have.property( 'x' ).and.equal( 1 );
+					done();
+				}
+			} );
+			Tweenkey.update( 1 );
+		} );
+
+		it( 'to: should execute onStart callback with all properties untouched', function( done ) {
+			var obj = { x: 0 };
+			Tweenkey.to( obj, 1, { x: 1, 
+				onStart: function( target ) {
+					expect( obj ).to.have.property( 'x' ).and.equal( 0 );
+					done();
+				}
+			} );
+			Tweenkey.update(1);
+		} );
+
 		it( 'from: should work with the wrong callback parameters', function() {
 			invalidCallbacks.forEach( function( val ) {
 				
@@ -180,6 +206,31 @@ describe( 'tweenkey', function() {
 				expect( tween ).to.respondTo( '_onUpdate' );
 				expect( tween._target ).to.have.property( 'x' ).and.equal( 0 );
 			} );
+		} );
+
+		it( 'from: execute the [onComplete, onUpdate] callbacks', function( done ) {
+			var obj = { x: 0 };
+			Tweenkey.from( obj, 1, { x: 1, 
+				onUpdate: function( target ) {
+					expect( obj ).to.have.property( 'x' ).and.equal( 0 );
+				},
+				onComplete: function( target ) {
+					expect( obj ).to.have.property( 'x' ).and.equal( 0 );
+					done();
+				}
+			} );
+			Tweenkey.update( 1 );
+		} );
+
+		it( 'from: should execute onStart callback with all properties untouched', function( done ) {
+			var obj = { x: 0 };
+			Tweenkey.to( obj, 1, { x: 1, 
+				onStart: function( target ) {
+					expect( obj ).to.have.property( 'x' ).and.equal( 0 );
+					done();
+				}
+			} );
+			Tweenkey.update(1);
 		} );
 
 		it( 'fromTo: should work with the wrong callback parameters', function() {
@@ -201,6 +252,74 @@ describe( 'tweenkey', function() {
 				expect( tween._target ).to.have.property( 'x' ).and.equal( 2 );
 			} );
 		} );
-		
+
+		it( 'fromTo: execute the [onComplete, onUpdate] callbacks', function( done ) {
+			var obj = { x: 0 };
+			Tweenkey.fromTo( obj, 1, {
+				x: 2
+			}, { 
+				x: 1, 
+				onUpdate: function( target ) {
+					expect( obj ).to.have.property( 'x' ).and.equal( 1 );
+				},
+				onComplete: function( target ) {
+					expect( obj ).to.have.property( 'x' ).and.equal( 1 );
+					done();
+				}
+			} );
+			Tweenkey.update( 1 );
+		} );
+
+		it( 'fromTo: should execute onStart callback with all properties untouched', function( done ) {
+			var obj = { x: 0 };
+			Tweenkey.fromTo( obj, 1, { x: 2 }, { x: 1, 
+				onStart: function( target ) {
+					expect( obj ).to.have.property( 'x' ).and.equal( 0 );
+					done();
+				}
+			} );
+			Tweenkey.update(1);
+		} );
+
+	} );
+
+	describe( 'Tween linear interpolation', function() {
+
+		it( 'to: should interpolate linear properties by default', function() {
+			var obj = { x: 0 };
+			Tweenkey.to( obj, 1, { x: 1 } );
+			for ( var i = 0; i < 10; i++ ) {
+				var expectedVal = i * 0.1;
+				// add tolerance to match property decimals between a range
+				expect( obj ).to.have.property( 'x' ).and.to.be.within( expectedVal - 0.0001,  expectedVal + 0.0001);
+				Tweenkey.update(0.1);
+			}
+		} );
+
+		it( 'from: should interpolate linear properties by default', function() {
+			var obj = { x: 1 };
+			Tweenkey.from( obj, 1, { x: 0 } );
+
+			// will trigger the update to start with the current object property x setted to 0
+			// this behaviour is inteded, user can change this with inmediate render param ?
+			Tweenkey.update(0.000001);
+
+			for ( var i = 0; i < 10; i++ ) {
+				var expectedVal = i * 0.1;
+				expect( obj ).to.have.property( 'x' ).and.to.be.within( expectedVal - 0.0001,  expectedVal + 0.0001);
+				Tweenkey.update(0.1);
+			}
+		} );
+
+		it( 'fromTo: should interpolate linear properties by default', function() {
+			var obj = { x: 0 };
+			Tweenkey.fromTo( obj, 1, { x: 0 }, { x: 1 } );
+			for ( var i = 0; i < 10; i++ ) {
+				var expectedVal = i * 0.1;
+				expect( obj ).to.have.property( 'x' ).and.to.be.within( expectedVal - 0.0001,  expectedVal + 0.0001);
+				Tweenkey.update(0.1);
+			}
+		} );
+
 	} );
 } );
